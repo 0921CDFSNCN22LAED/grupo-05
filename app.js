a62a2eba8b62771343c493f914a69ec282648cf4
Enzo L. Baitelo <enzo@Enzos-MacBook-Air.local>
2021-10-27 00:10:19 -0300

    agregando app.get para cava

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const publicPath = path.join(__dirname , './public')
+app.use(express.static(publicPath))
 
 app.listen(3000 , () =>{
     console.log('Servidor funciona')
@@ -10,3 +12,7 @@ app.listen(3000 , () =>{
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
   });
+
+app.get("/cava" , (req , res) => {
+    res.sendFile(path.join(__dirname, 'views/cava.html'))
+})
