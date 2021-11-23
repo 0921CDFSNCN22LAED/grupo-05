a19c6343e87c88d24b07bb39d288c4ac5399e83b
Enzo L. Baitelo <enzo@Enzos-MacBook-Air.local>
2021-11-23 18:50:56 -0300

    cambio a forms

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const mainRouters = require ("./routers/mainRouters.js")
+const mainRouters = require("./routers/mainRouters.js");
+const methodOverride = require('method-override');
 
 const publicPath = path.join(__dirname, "./public");
 app.use(express.static(publicPath));
@@ -13,3 +14,6 @@ app.listen(3000, () => {
 app.set('view engine', 'ejs');
 
 app.use ("/", mainRouters);
+app.use(express.urlencoded({ extended : false}));
+app.use(express.json());
+app.use(methodOverride('_method'))
