4c8807eea8ca88a6af0ac58038d7b3ef9fe1cbcd
FranciscoBisso <fjbiss@gmail.com>
2021-12-05 13:34:12 -0300

    elimino imgs inutiles. Actualizo vista de vinoteca

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,11 +2,11 @@ productsService = require("../services/productsServices");
 
 vinos = productsService.getAll();
 
-
 const productsController = {
   cava: (req, res) => {
     res.render("users/cava");
   },
+
   detalleProducto: (req, res) => {
     const id = req.params.id;
     const vino = productsService.findOne(id);
@@ -24,7 +24,7 @@ const productsController = {
     res.render("products/agregarProducto");
   },
   store: (req, res) => {
-    productsService.createOne (req.body);
+    productsService.createOne(req.body);
     res.redirect("/products");
   },
 
@@ -40,25 +40,24 @@ const productsController = {
       res.send("No seleccionaste ningun vino. Intenta /editarProductos/2");
     }
   },
+
   actualizarProducto: (req, res) => {
     const id = req.params.id;
-    const index = vinos.findIndex((vino)=>{
-			return vino.id == id;
-		})
+    const index = vinos.findIndex((vino) => {
+      return vino.id == id;
+    });
 
     const updatedProduct = {
       id: vinos[index].id,
-      imagen: '/images/img-products/'+req.file.filename, 
-      ...req.body
-    }
+      imagen: "/images/img-products/" + req.file.filename,
+      ...req.body,
+    };
 
     vinos[index] = updatedProduct;
-    
 
-    productsService.saveProducts()
-    
-    
-    res.redirect('/products/detalle/' + updatedProduct.id)
+    productsService.saveProducts();
+
+    res.redirect("/products/detalle/" + updatedProduct.id);
   },
 
   eliminarProducto: (req, res) => {
