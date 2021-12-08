adc5e1f4219196a5ec9663f4ee9327c2d782b96e
Fede Re <fede90.re@gmail.com>
2021-12-08 15:19:35 -0300

    cambio for home y editar productos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,56 +1,12 @@
 const path = require("path");
 
-const vinos = [
-  {
-    id: 1,
-    nombre: "Vino 1",
-    bodega: "",
-    descripcion: "Cabernet 2018",
-    precio: "$1000",
-    imagen: "/images/vino1.jpeg",
-    categoria: "",
-  },
-  {
-    id: 2,
-    nombre: "Vino 2",
-    bodega: "",
-    descripcion: "Malbec 2019",
-    precio: "$1000",
-    imagen: "/images/vino2.jpeg",
-    categoria: "",
-  },
-  {
-    id: 3,
-    nombre: "Vino 3",
-    bodega: "",
-    descripcion: "Malbec 2016",
-    precio: "$1000",
-    imagen: "/images/vino3.jpeg",
-    categoria: "",
-  },
-  {
-    id: 4,
-    nombre: "Vino 4",
-    bodega: "",
-    descripcion: "Cabernet 2010",
-    precio: "$1000",
-    imagen: "/images/vino1.jpeg",
-    categoria: "",
-  },
-  {
-    id: 5,
-    bodega: "",
-    nombre: "Vino 5",
-    descripcion: "Malbec 2012",
-    precio: "$1000",
-    imagen: "/images/vino1.jpeg",
-    categoria: "",
-  }
-];
+const productsService = require("../services/productsServices");
+
+const vinos = productsService.getAll();
 
 const controladorHome = {
   index: (req, res) => {
-    res.render("users/index");
+    res.render("users/index", { vinos });
   },
 
   registro: (req, res) => {
