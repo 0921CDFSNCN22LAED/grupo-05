d09b64c8be495d6b77ebc4caf2df3f75f2b33e56
FranciscoBisso <fjbiss@gmail.com>
2021-12-09 21:31:01 -0300

    creamos carpeta src

--- src/app.js
+++ src/app.js
@@ -1,30 +1,23 @@
-
 const express = require("express");
 const app = express();
 const path = require("path");
 const mainRouters = require("./routers/mainRouters.js");
-const productsRouters = require("./routers/productsRouters.js")
-const methodOverride = require('method-override');
+const productsRouters = require("./routers/productsRouters.js");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-const publicPath = path.join(__dirname, "./public");
+const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
-
-
 
 app.listen(3000, () => {
   console.log("Servidor funciona");
 });
 
-
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'))
-
-
-
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 app.use("/", mainRouters);
 app.use("/products", productsRouters);
