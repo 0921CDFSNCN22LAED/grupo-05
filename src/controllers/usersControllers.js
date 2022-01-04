53eb1984a7a4912889546ef98f4575273e64350b
enzdor <enzo01lb@gmail.com>
2022-01-04 15:39:36 -0300

    .

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -9,44 +9,6 @@ const usersController = {
   registro: (req, res) => {
     res.render("users/register");
   },
-  accountStore: (req, res) => {
-    let errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      let accountDb = accountsService.findByField('email', req.body.email)
-      
-      if (accountDb){
-          errors.errors.push({
-              value: req.body.email,
-              msg: 'Este mail ya esta en uso',
-              param: 'email',
-              location: 'body'
-          })
-          res.render('users/register',{errors: errors.errors})
-          return
-      }
-      let account = {
-          id: Date.now(),
-          name: req.body.name,
-          email: req.body.email,
-          birthday: req.body.birthday,
-          dni: req.body.dni,
-          password: bcryptjs.hashSync(req.body.password, 10),
-          category: 'user'
-      };
-
-      accounts.push(account);
-      accountsService.saveAccounts();
-      
-      let userLogin = accountsService.findByField('email', req.body.email);
-      res.redirect('/users/cuenta');
-
-
-  } else {
-      res.render('users/register',
-      {errors: errors.errors, old: req.body})
-  }
-  },
   login: (req, res) => {
     res.render("users/login");
   },
