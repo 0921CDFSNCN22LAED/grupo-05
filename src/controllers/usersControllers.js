c4d63d897fe57e3a96ee2a28bab02b5d35325a24
FranciscoBisso <fjbiss@gmail.com>
2022-01-11 15:56:15 -0300

    Creo usuario de prueba. Agrego middleware ifLogged

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,80 +1,88 @@
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 
-const accountsService = require('../services/accountsServices.js');
+const accountsService = require("../services/accountsServices.js");
 const accounts = accountsService.getAll();
 
-const { validationResult } = require('express-validator');
-const { redirect } = require('express/lib/response');
+const { validationResult } = require("express-validator");
+const { redirect } = require("express/lib/response");
 
 const usersController = {
-  registro: (req, res) => {
-    res.render("users/register");
-  },
-  registroProcesado: (req, res) => {
-    let errors = validationResult(req);
+    registro: (req, res) => {
+        res.render("users/register");
+    },
+    registroProcesado: (req, res) => {
+        let errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-      let usuarioDb = accountsService.findByField("email", req.body.email)
-      if (usuarioDb) {
-        errors.errors.push({
-          value: req.body.email,
-          msg: "Ya existe un usuario con ese email",
-          param: "email",
-          location: "body"
-        })
-        res.render("users/register", { msg: errors.errors })
-      };
-      let newAccount = {
-        id: Date.now(),
-        name: req.body.name,
-        email: req.body.email,
-        birthday: req.body.birthday,
-        dni: req.body.dni,
-        password: bcryptjs.hashSync(req.body.password, 10),
-        category: "user"
-      }
-      accounts.push(newAccount);
-      accountsService.saveAccounts();
-      res.redirect("/");
-    }
-    else {
-      res.render("users/register", { old: req.body, msg: errors.errors })
-    }
-  },
-  login: (req, res) => {
-    res.render("users/login");
-  },
-  loginProcess: (req, res) => {
-    let errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      let userLogin = accountsService.findByField('email', req.body.email)
-
-      if (userLogin) {
-        let passwordOk = bcryptjs.compareSync(req.body.password, userLogin.password);
-        if (passwordOk) {
-          res.redirect('/users/cuenta');
-          return
+        if (errors.isEmpty()) {
+            let usuarioDb = accountsService.findByField(
+                "email",
+                req.body.email
+            );
+            if (usuarioDb) {
+                errors.errors.push({
+                    value: req.body.email,
+                    msg: "Ya existe un usuario con ese email",
+                    param: "email",
+                    location: "body",
+                });
+                res.render("users/register", { msg: errors.errors });
+            }
+            let newAccount = {
+                id: Date.now(),
+                name: req.body.name,
+                email: req.body.email,
+                birthday: req.body.birthday,
+                dni: req.body.dni,
+                password: bcryptjs.hashSync(req.body.password, 10),
+                category: "user",
+            };
+            accounts.push(newAccount);
+            accountsService.saveAccounts();
+            res.redirect("/");
+        } else {
+            res.render("users/register", { old: req.body, msg: errors.errors });
         }
-        else {
-          errors.errors.push({
-            value: req.body.email,
-            msg: 'La contraseña es incorrecta',
-            param: 'password',
-            location: 'body'
-          })
-          res.render('users/login', {
-            msg: errors.errors
-          })
-          return
-        }
-      }
-    }
-  },
-  cuenta: (req, res) => {
-    res.render('users/cuenta')
-  }
+    },
+    login: (req, res) => {
+        res.render("users/login");
+    },
+    loginProcess: (req, res) => {
+        let errors = validationResult(req);
 
-}
+        if (errors.isEmpty()) {
+            let userLogin = accountsService.findByField(
+                "email",
+                req.body.email
+            );
+
+            if (userLogin) {
+                let passwordOk = bcryptjs.compareSync(
+                    req.body.password,
+                    userLogin.password
+                );
+                if (passwordOk) {
+                    delete userLogin.password;
+                    req.session.loggedUser = userLogin;
+                    res.redirect("/users/cuenta");
+                    return;
+                } else {
+                    errors.errors.push({
+                        value: req.body.email,
+                        msg: "La contraseña es incorrecta",
+                        param: "password",
+                        location: "body",
+                    });
+                    res.render("users/login", {
+                        msg: errors.errors,
+                    });
+                    return;
+                }
+            }
+        }
+    },
+    cuenta: (req, res) => {
+        res.render("users/cuenta");
+    },
+};
 
 module.exports = usersController;
--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -3,57 +3,70 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const usersController = require("../controllers/usersControllers");
+const ifLogged = require("../Middlewares/ifLogged");
 
 //guardado de archivos de multer
 const storage = multer.diskStorage({
     destination: (req, res, cb) => {
-        cb(null, path.join(__dirname, "../public/images/img-users"))
+        cb(null, path.join(__dirname, "../public/images/img-users"));
     },
     filename: (req, res, cb) => {
         const newFilename = "user-" + Date.now();
         cb(null, newFilename);
-    }
+    },
 });
 
 const upload = multer({ storage: storage });
 
 /* EXPRESS VALIDATOR PARA ACCOUNT*/
 
-const { check } = require('express-validator');
+const { check } = require("express-validator");
 
 const { body } = require("express-validator");
+const req = require("express/lib/request");
 
 let validateAccountLogin = [
-    check('email').notEmpty().withMessage('Debes completar el mail'),
-    check('password').notEmpty().withMessage('Debes completar la contrasena')
-]
+    check("email").notEmpty().withMessage("Debes completar el mail"),
+    check("password").notEmpty().withMessage("Debes completar la contrasena"),
+];
 
 let validateAccountRegister = [
     check("name").notEmpty().withMessage("Debes completar tu nombre"),
-    check("email").notEmpty().withMessage("Debes completar el mail").isEmail().withMessage("Debes ingresar un email valido"),
-    check("birthday").notEmpty().withMessage("Debes completar tu fecha de cumpleaños"),
+    check("email")
+        .notEmpty()
+        .withMessage("Debes completar el mail")
+        .isEmail()
+        .withMessage("Debes ingresar un email valido"),
+    check("birthday")
+        .notEmpty()
+        .withMessage("Debes completar tu fecha de cumpleaños"),
     check("dni").notEmpty().withMessage("Debes completar con tu dni"),
-    check("imagen-usuario").notEmpty().withMessage("Completa con una imagen valida"),
-    check("password").notEmpty().withMessage("Debes completar con tu contraseña"),
+    check("imagen-usuario")
+        .notEmpty()
+        .withMessage("Completa con una imagen valida"),
+    check("password")
+        .notEmpty()
+        .withMessage("Debes completar con tu contraseña"),
     body("confirm-password").custom((value, { req }) => {
         if (value != req.body.password) {
-            throw new Error("Tus contraseñas deben ser iguales")
+            throw new Error("Tus contraseñas deben ser iguales");
         }
         return true;
-    })
-]
+    }),
+];
 
+router.get("/registro", ifLogged, usersController.registro);
+router.post(
+    "/registro",
+    upload.single("imagen-usuario"),
+    usersController.registroProcesado
+);
 
-
-router.get("/registro", usersController.registro);
-router.post("/registro", upload.single("imagen-usuario"), usersController.registroProcesado);
-
-router.get("/login", usersController.login);
+router.get("/login", ifLogged, usersController.login);
 router.post("/login", usersController.loginProcess);
-
 
 router.get("/cuenta", usersController.cuenta);
 
-router.post("/login", validateAccountLogin, usersController.loginProcess)
+router.post("/login", validateAccountLogin, usersController.loginProcess);
 
 module.exports = router;
