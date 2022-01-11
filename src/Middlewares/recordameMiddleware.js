dd1d849e7394016e3443028aa51b285478b24b50
FranciscoBisso <fjbiss@gmail.com>
2022-01-11 20:34:43 -0300

    probando con cookies

--- src/Middlewares/recordameMiddleware.js
+++ src/Middlewares/recordameMiddleware.js
@@ -1,21 +1,21 @@
-const user = require('../services/usersServices');
-
+//const user = require('../services/usersServices');
+const accountsService = require("../services/accountsServices");
 
 function recordameMiddleware(req, res, next) {
     res.locals.isLogged = false;
 
     let emailInCookie = req.cookies.userEmail;
-	let userFromCookie = User.findByField('email', emailInCookie);
+    let userFromCookie = accountsService.findByField("email", emailInCookie);
 
-	if (userFromCookie) {
-		req.session.userLogged = userFromCookie;
-	}
+    if (userFromCookie) {
+        req.session.loggedUser = userFromCookie;
+    }
 
-	if (req.session.userLogged) {
-		res.locals.isLogged = true;
-		res.locals.userLogged = req.session.userLogged;
-	}
+    if (req.session.loggedUser) {
+        res.locals.isLogged = true;
+        res.locals.loggedUser = req.session.loggedUser;
+    }
 
-	next();
+    next();
 }
 module.exports = recordameMiddleware;
