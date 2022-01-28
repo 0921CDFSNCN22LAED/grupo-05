4f7b990d69098cf0a2e103538277c17d7b2bc882
Fede Re <fede90.re@gmail.com>
2022-01-28 11:59:42 -0300

    agregamos hojas de modelos y tablas vino

---
+++ src/database/models/Vino.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, dataTypes) => {
+    const Vino = sequelize.define(
+        "Vinos",
+        {
+            id: {
+                type: dataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            nombre: {
+                type: dataTypes.STRING(600),
+                allowNull: false
+            },
+            bodega_id: {
+                type: dataTypes.INTEGER,
+                allowNull: false
+            },
+            anio: {
+                type: dataTypes.DATE,
+                allowNull: false
+            },
+            descripcion: {
+                type: dataTypes.STRING(600),
+                allowNull: true
+            },
+            precio: {
+                type: dataTypes.INTEGER,
+                allowNull: false
+            },
+            imagen: {
+                type: dataTypes.STRING(200),
+                allowNull: false
+            },
+            stock: {
+                type: dataTypes.INTEGER,
+                allowNull: false
+            },
+            uva_id: {
+                type: dataTypes.INTEGER,
+                allowNull: false
+            },
+            categoria_id: {
+                type: dataTypes.INTEGER,
+                allowNull: false
+            }
+
+        },
+        {
+            tableName: "Vinos",
+            timesTamps: false
+        }
+    );
+    return Vino
+}
