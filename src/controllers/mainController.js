f136b0468ee6532ccf57d8ed335b6db47e845d26
FranciscoBisso <fjbiss@gmail.com>
2022-02-10 13:44:54 -0300

    corrijo errores del mainController. Modifico vista de index para que acompañe los cambios del controllador

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,27 +1,16 @@
-const productsService = require("../services/productsServices");
-const vinos = productsService.getAll();
 const db = require("../database/models");
 
 const controladorHome = {
     index: async (req, res) => {
-        //res.render("users/index", { vinos }); viejo crud
-        db.Vinos.findAll()
-            .then((vinos) => {
-                res.render("users/index", {
-                    vinos: vinos,
-                    link: "/editarProductos/" + vinos.id,
-                });
-            })
-            .catch((error) => {
-                console.log(error);
+        try {
+            const vinos = await db.Vinos.findAll();
+            res.render("users/index", {
+                vinos: vinos,
+                link: "/detalle/" + vinos.id,
             });
-
-        //probando con async await. agregar async.
-        /*const vinos = await db.Vinos.findAll();
-        res.render("users/index", {
-            vinos: vinos,
-            link: "/editarProductos/" + vinos.id,
-        });*/
+        } catch (error) {
+            console.error(error);
+        }
     },
 };
 
