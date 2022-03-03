1ee7f30d6aef1e02db67deb9052a58fabe4e63d2
FranciscoBisso <fjbiss@gmail.com>
2022-03-03 12:01:30 -0300

    creo nuevo middleware de multer para registro de usuarios

--- website/src/Middlewares/validaciones/backend/validateAccountRegister.js
+++ website/src/Middlewares/validaciones/backend/validateAccountRegister.js
@@ -1,5 +1,5 @@
 /* EXPRESS VALIDATOR PARA ACCOUNT*/
-
+const path = require("path");
 const { check } = require("express-validator");
 const { body } = require("express-validator");
 
@@ -16,6 +16,24 @@ let validateAccountRegister = [
     body("confirm-password").custom((value, { req }) => {
         if (value != req.body.password) {
             throw new Error("Tus contraseñas deben ser iguales");
+        }
+        return true;
+    }),
+    body("imagen").custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = [".jpg", ".png", ".jpeg", ",gif"];
+
+        if (!file) {
+            throw new Error("Tienes que subir una foto de perfil.");
+        } else {
+            let fileExtension = path.extname(file.originalname);
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error(
+                    `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+                        ", "
+                    )}`
+                );
+            }
         }
         return true;
     }),
