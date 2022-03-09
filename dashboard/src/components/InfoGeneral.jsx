6c69a5222a5667fe65a99382595631797cc964b3
enzdor <enzo01lb@gmail.com>
2022-03-08 23:43:44 -0300

    render de cards ahora no es medieval

--- dashboard/src/components/InfoGeneral.jsx
+++ dashboard/src/components/InfoGeneral.jsx
@@ -1,59 +1,57 @@
 import React, { Component } from "react";
 import InfoGeneralCard from "./InfoGeneralCard";
 
-const metrics = [
-    {
-        borderColor: "primary",
-        titleColor: "primary",
-        title: "Vinos en la Base de Datos",
-    },
-    {
-        borderColor: "success",
-        titleColor: "success",
-        title: "Bodegas en la Base de Datos",
-    },
-    {
-        borderColor: "warning",
-        titleColor: "warning",
-        title: "Uvas en la Base de Datos",
-    },
-    {
-        borderColor: "danger",
-        titleColor: "danger",
-        title: "Usuarios en la Base de Datos",
-    },
-];
 
 export default class InfoGeneral extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            bodegasNumber: 0,
-            vinosNumber: 0,
-            uvasNumber: 0,
-            usuariosNumber: 0,
+            metrics: [
+                {
+                    borderColor: "primary",
+                    titleColor: "primary",
+                    title: "Vinos en la Base de Datos",
+                    number: 0
+                },
+                {
+                    borderColor: "success",
+                    titleColor: "success",
+                    title: "Bodegas en la Base de Datos",
+                    number: 0
+                },
+                {
+                    borderColor: "warning",
+                    titleColor: "warning",
+                    title: "Uvas en la Base de Datos",
+                    number: 0
+                },
+                {
+                    borderColor: "danger",
+                    titleColor: "danger",
+                    title: "Usuarios en la Base de Datos",
+                    number: 0
+                }
+            ]
         };
     }
 
     async getNumbers() {
         const dataVinos = await fetch("http://localhost:3001/api/vinos");
         const dataVinosJson = await dataVinos.json();
-        this.setState({
-            vinosNumber: dataVinosJson.meta.total,
-        });
-        this.setState({
-            bodegasNumber: Object.keys(dataVinosJson.countByCategory.bodegas)
-                .length,
-        });
-        this.setState({
-            uvasNumber: Object.keys(dataVinosJson.countByCategory.uvas).length,
-        });
-
         const dataUsuarios = await fetch("http://localhost:3001/api/usuarios");
         const dataUsuariosJson = await dataUsuarios.json();
+        const newMetrics = this.state.metrics
+
+        newMetrics[0].number = dataVinosJson.meta.total
+        newMetrics[1].number = Object.keys(dataVinosJson.countByCategory.bodegas).length
+        newMetrics[2].number = Object.keys(dataVinosJson.countByCategory.uvas).length
+        newMetrics[3].number = dataUsuariosJson.meta.total
+        
         this.setState({
-            usuariosNumber: dataUsuariosJson.meta.total,
+            metrics: newMetrics,
         });
+
+        console.log(this.state);
     }
 
     componentDidMount() {
@@ -63,30 +61,14 @@ export default class InfoGeneral extends Component {
     render() {
         return (
             <div className="row">
-                <InfoGeneralCard
-                    borderColor={metrics[0].borderColor}
-                    titleColor={metrics[0].titleColor}
-                    title={metrics[0].title}
-                    number={this.state.vinosNumber}
-                />
-                <InfoGeneralCard
-                    borderColor={metrics[1].borderColor}
-                    titleColor={metrics[1].titleColor}
-                    title={metrics[1].title}
-                    number={this.state.bodegasNumber}
-                />
-                <InfoGeneralCard
-                    borderColor={metrics[2].borderColor}
-                    titleColor={metrics[2].titleColor}
-                    title={metrics[2].title}
-                    number={this.state.uvasNumber}
-                />
-                <InfoGeneralCard
-                    borderColor={metrics[3].borderColor}
-                    titleColor={metrics[3].titleColor}
-                    title={metrics[3].title}
-                    number={this.state.usuariosNumber}
-                />
+                {this.state.metrics.map((metric)=> (
+                    <InfoGeneralCard 
+                    borderColor={metric.borderColor}
+                    titleColor={metric.titleColor}
+                    title={metric.title}
+                    number={metric.number}
+                    />
+                ))}
             </div>
         );
     }
